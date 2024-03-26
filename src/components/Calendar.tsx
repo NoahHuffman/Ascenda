3ffa4591843134b3c51669f6dbaa4ce5436156bb
Noah Huffman <nhuffman@doveresg.com>
2024-03-26 11:32:17 -0400

    Add JSON file, create Add Item button

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -4,11 +4,22 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 
 const Calendar = () => {
+  const addItem = () =>{
+    alert('clicked');
+  }
+
   return (
     <FullCalendar
       plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
       initialView={"dayGridWeek"}
+      customButtons={{
+        addItemBtn: {
+          text: 'Add Item',
+          click: addItem,
+        },
+      }}
       headerToolbar={{
+        left: 'today prev,next addItemBtn',
         start: "today prev,next",
         center: "title",
         end: "dayGridMonth, timeGridWeek, timeGridDay",
